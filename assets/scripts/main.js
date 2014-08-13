1af4dfd3001d55666d6c8094cf6d709b3e6e1551
Marlon Baeten <marlon@tweedegolf.com>
2014-08-13 17:16:12 +0200

    Added file upload dialog

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,38 +2,99 @@ var $ = require('jquery');
 var Ractive = require('ractive');
 var fs = require('fs');
 
-$(function () {
+var modal = null;
+
+var handle_upload = function () {
+    var file = this.files[0];
+    var client = new XMLHttpRequest();
+    var data = new FormData();
+
+    data.append('upload', file);
+    client.open('post', '/api/upload', true);
+    client.setRequestHeader('Content-Type', 'multipart/form-data');
+    client.send(data);
+
+    client.addEventListener('progress', function (oEvent) {
+        if (oEvent.lengthComputable) {
+            var percentComplete = oEvent.loaded / oEvent.total;
+            // ...
+        } else {
+            // Unable to compute progress information since the total size is unknown
+        }
+    }, false);
+
+    client.addEventListener('load', function (evt) {
+        alert('The transfer is complete.');
+    }, false);
+
+    client.addEventListener('error', function transferFailed(evt) {
+        alert('An error occurred while transferring the file.');
+    }, false);
+
+    client.addEventListener('abort', function transferFailed(evt) {
+        alert('The transfer has been canceled by the user.');
+    }, false);
+};
+
+var create_modal = function (on_select) {
+
+    var elem = document.createElement('div');
+    elem.className = 'modal';
+    elem.id = 'tg-media-modal';
+
+    var input = document.createElement('input');
+    input.type = 'file';
+
+    input.addEventListener('change', handle_upload, true);
 
     var i, data = [];
     for (i = 0; i < 10; i += 1) {
         data.push({
-            'image': 'http://lorempixel.com/600/480/animals/' + i,
-            'thumb': 'http://lorempixel.com/200/150/animals/' + i,
+            'image': 'http://lorempixel.com/600/480/animals/' + i % 10,
+            'thumb': 'http://lorempixel.com/200/150/animals/' + i % 10,
             'name': 'kat-' + i + '.jpg'
         });
     }
 
-    console.log(data);
-
     var ractive = new Ractive({
-        // The `el` option can be a node, an ID, or a CSS selector.
-        el: 'modal',
-
-        // We could pass in a string, but for the sake of convenience
-        // we're passing the ID of the <script> tag above.
+        el: elem,
         template: fs.readFileSync(__dirname + '/templates/browser.html', 'utf8'),
-
-        // Here, we're passing in some initial data
         data: {
             showForm: false,
             images: data
         }
     });
 
-    ractive.on('showForm', function (event) {
-        this.set('showForm', true);
+    ractive.on('select', function (event) {
+        event.original.preventDefault();
+        on_select(event.node.href);
     });
 
-});
+    ractive.on('upload', function (event) {
+        event.original.preventDefault();
+        input.click();
+
+    });
 
 
+
+    return elem;
+};
+
+
+exports.tinymce_callback = function (field_name) {
+
+    if (modal === null) {
+
+        modal = create_modal(function (url) {
+            $('#' + field_name).val(url);
+            $(modal).modal('hide');
+        });
+
+        $(modal).modal();
+
+    } else {
+
+        $(modal).modal('show');
+    }
+};
